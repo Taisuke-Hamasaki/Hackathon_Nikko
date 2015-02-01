64cadda68e93528ffa3aef1081d35301949bfe27
mt146510 <mt146510@gmail.com>
2015-02-01 14:46:46 +0900

    nikko

--- NikkoBus/NikkoBus/BeaconIO.swift
+++ NikkoBus/NikkoBus/BeaconIO.swift
@@ -29,7 +29,7 @@ class BeaconIO: UIViewController,UITableViewDelegate, UITableViewDataSource, CLL
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        DEBUG = false;
+        DEBUG = true;
         
         // ロケーションマネージャの作成.
         myLocationManager = CLLocationManager()
@@ -103,6 +103,24 @@ view.backgroundColor = UIColorFromRGB(0x339999)
         
         // ボタンを追加する.
         self.view.addSubview(myButtonNext);
+        }
+        if(DEBUG == false){
+                //super.viewDidLoad()
+                
+                // UIImageViewを作成する.
+                let myImageView: UIImageView = UIImageView(frame: CGRectMake(0,0,180,240))
+                
+                // 表示する画像を設定する.
+                let myImage = UIImage(named: "title_img.png")
+                
+                // 画像をUIImageViewに設定する.
+                myImageView.image = myImage
+                
+                // 画像の表示する座標を指定する.
+                myImageView.layer.position = CGPoint(x: self.view.bounds.width/2, y: 300.0)
+                
+                // UIImageViewをViewに追加する.
+                self.view.addSubview(myImageView)
         }
         
     }
