e740e328de2a07254998dfb3fc7fe5f3f878acc4
mt146510 <mt146510@gmail.com>
2015-02-01 11:03:41 +0900

    NikkoHackathon

---
+++ NikkoBus/NikkoBus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NikkoBus
+//
+//  Created by 字大　太郎 on 2015/02/01.
+//  Copyright (c) 2015年 字大　太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ NikkoBus/NikkoBusTests/NikkoBusTests.swift
@@ -0,0 +1,36 @@
+//
+//  NikkoBusTests.swift
+//  NikkoBusTests
+//
+//  Created by 字大　太郎 on 2015/02/01.
+//  Copyright (c) 2015年 字大　太郎. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class NikkoBusTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
